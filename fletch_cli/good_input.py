b402cc6b373130f5b5ae302ec7c15c29cc93a591
fletchgraham <sfletchgraham@gmail.com>
2018-09-13 00:03:42 -0400

    add keyboard interrupt handling to input function

--- fletch_cli/good_input.py
+++ fletch_cli/good_input.py
@@ -5,46 +5,50 @@ def get_input(prompt="Type something", expected_type="String"):
     prompt += ' >>>'
     answer = ''
 
-    while True:
-        answer = str(input(prompt))
+    try:
+        while True:
+            answer = str(input(prompt))
 
-        if answer == '':
-            print("You didn't type anything.")
+            if answer == '':
+                print("You didn't type anything.")
 
-        elif expected_type.lower() in ['str', 'string']:
-            break
-
-        elif expected_type.lower() in ['int', 'integer']:
-            try:
-                answer = int(answer)
-                break
-            except:
-                print("that wasn't an Integer.")
-
-        elif expected_type.lower() in ['val', 'value', 'float']:
-            try:
-                answer = float(answer)
-                break
-            except:
-                print("that wasn't a Float.")
-
-        elif expected_type.lower() in ['bool', 'boolean', 'tf', 'yn']:
-            answer = str(answer)
-            if answer.lower() in ['y', 'yes', 'true', 't', '1']:
-                answer = True
+            elif expected_type.lower() in ['str', 'string']:
                 break
 
-            elif answer.lower() in ['n', 'no', 'false', 'f', '0']:
-                answer = False
-                break
+            elif expected_type.lower() in ['int', 'integer']:
+                try:
+                    answer = int(answer)
+                    break
+                except:
+                    print("that wasn't an Integer.")
+
+            elif expected_type.lower() in ['val', 'value', 'float']:
+                try:
+                    answer = float(answer)
+                    break
+                except:
+                    print("that wasn't a Float.")
+
+            elif expected_type.lower() in ['bool', 'boolean', 'tf', 'yn']:
+                answer = str(answer)
+                if answer.lower() in ['y', 'yes', 'true', 't', '1']:
+                    answer = True
+                    break
+
+                elif answer.lower() in ['n', 'no', 'false', 'f', '0']:
+                    answer = False
+                    break
+
+                else:
+                    print("Couldn't use that as a boolean.")
 
             else:
-                print("Couldn't use that as a boolean.")
+                print("Try Again.")
 
-        else:
-            print("Try Again.")
+        return answer
 
-    return answer
+    except KeyboardInterrupt:
+        exit()
 
 if __name__ == "__main__":
     print(get_input('Type stuff to test', 'bool'))
