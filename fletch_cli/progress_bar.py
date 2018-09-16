be5b6af190686d5eb4593710941a1502f8ad0ef6
fletchgraham <sfletchgraham@gmail.com>
2018-09-16 13:07:17 -0400

    make progress bar use full blocks if the terminal can print it, otherwise it'll use hash symbols.

--- fletch_cli/progress_bar.py
+++ fletch_cli/progress_bar.py
@@ -8,13 +8,18 @@ def print_progress(iteration, total):
     prefix = 'Progress'
     suffix = 'Complete'
     length = 50
-    fill = '|'
+    fill = u"\u2588"
+    fill_alt = '#'
 
     percent = ("{0:.1f}").format(100 * (iteration / float(total)))
     filledLength = int(length * iteration // total)
     bar = fill * filledLength + '-' * (length - filledLength)
+    bar_alt = fill_alt * filledLength + '-' * (length - filledLength)
 
-    sys.stdout.write('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix))
+    try:
+        sys.stdout.write('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix))
+    except:
+        sys.stdout.write('\r%s |%s| %s%% %s' % (prefix, bar_alt, percent, suffix))
     sys.stdout.flush()
 
     # Print New Line on Complete
